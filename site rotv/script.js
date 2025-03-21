629bfaf0da7017e7b93dced3675bf38952201d9d
Andrei Lupau <93196728+andreilupau@users.noreply.github.com>
2025-03-21 21:45:24 +0200

    Add files via upload

---
+++ site rotv/script.js
@@ -0,0 +1,11 @@
+// Hamurger menu
+/*
+const hamMenu = document.querySelector(".ham-menu");
+
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
+*/
